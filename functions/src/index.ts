e129ccc5ef7bdc432975781efadc54a2518b5a09
FrazileDevelopers <frazile.com@gmail.com>
2018-12-15 15:36:36 +0530

    Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,32 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp()
 
+export const getUSServerUpdate = functions.firestore.document('servers/US').onUpdate(change => {
+    const after = change.after.data()
+    const payload = {
+        data: {
+            currency: after.currency,
+            period: String(after.period),
+            periodterms: after.periodterms,
+            price: String(after.price),
+            symbol: after.symbol
+        }
+    }
+    admin.messaging().sendToTopic("server_US", payload)
+    .catch(error => {
+        console.error('FCM failed: ', error)
+    })
+})
+
 export const getUSServer = functions.https.onRequest((request, response) => {
-    const promise = admin.firestore().doc('servers/US').get()
-    promise.then(snapshot => {
+    admin.firestore().doc('servers/US').get()
+    .then(snapshot => {
         const data = snapshot.data()
         response.send(data)
+    })
+    .catch(error => {
+        // Handle the error
+        console.log(error)
+        response.status(500).send(error)
     })
 });
