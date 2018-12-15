e3cec2644dd924e7c7551cb5589be58a05d78f60
FrazileDevelopers <frazile.com@gmail.com>
2018-12-15 23:00:14 +0530

    Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,20 +2,46 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp()
 
-export const getUSServerUpdate = functions.firestore.document('servers/US').onUpdate(change => {
-    const after = change.after.data()
-    const payload = {
-        data: {
-            currency: after.currency,
-            period: String(after.period),
-            periodterms: after.periodterms,
-            price: String(after.price),
-            symbol: after.symbol
+// export const getUSServerUpdate = functions.firestore.document('servers/US').onUpdate(change => {
+//     const after = change.after.data()
+//     const payload = {
+//         data: {
+//             currency: after.currency,
+//             period: String(after.period),
+//             periodterms: after.periodterms,
+//             price: String(after.price),
+//             symbol: after.symbol
+//         }
+//     }
+//     admin.messaging().sendToTopic("server_US", payload)
+//     .catch(error => {
+//         console.error('FCM failed: ', error)
+//     })
+// })
+
+export const getUSServerActiveServer = functions.https.onRequest((request, response) => {
+    admin.firestore().doc('servers-list/activeservers').get()
+    .then(activeserverSnapshot => {
+        const activeservers = activeserverSnapshot.data().servers
+        const activeserversarray = []
+        for (const server in activeservers) {
+            const p = admin.firestore().doc(`servers/${server}`).get()
+            activeserversarray.push(p)
         }
-    }
-    admin.messaging().sendToTopic("server_US", payload)
+        return Promise.all(activeserversarray)
+    })
+    .then(serverSnapshots => {
+        const results = []
+        serverSnapshots.forEach(serverSnap => {
+            const data = serverSnap.data()
+            data.server = serverSnap.id
+            results.push(data)
+        })
+        response.send(results)
+    })
     .catch(error => {
-        console.error('FCM failed: ', error)
+        console.log(error)
+        response.status(500).send(error)
     })
 })
 
